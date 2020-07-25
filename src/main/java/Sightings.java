8b45864998c7c275a3e7710b020091e9a33d87e1
BTG001 <brunokipkorir2@gmail.com>
2020-07-25 16:00:36 +0300

    create if statements to ensure the animal instatiates with name, age and health and that an exceptionis thrown if there is an error

--- src/main/java/Sightings.java
+++ src/main/java/Sightings.java
@@ -4,5 +4,16 @@ import java.sql.Timestamp;
 import java.util.List;
 
 public class Sightings implements AnimalInterface{
-
+    private String name;
+    private String location;
+    private int animalId;
+    private Timestamp timestamp;
+    private int id;
+    public Sightings(String name, String location, int animalId) {
+        if (name.equals("")) {
+            throw new IllegalArgumentException("Please enter a name mate");
+        }
+        if (location.equals("")) {
+            throw new IllegalArgumentException("Please enter a location mate");
+        }
 }

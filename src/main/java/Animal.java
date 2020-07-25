493ae82c3a3bd88e844498cbfcce5a429b88486d
BTG001 <brunokipkorir2@gmail.com>
2020-07-25 15:34:11 +0300

    create if statements to ensure the animal instatiates with name, age and health and that an exception is thrown incase of an error

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -3,5 +3,15 @@ import org.sql2o.Connection;
 import java.util.List;
 
 public class Animal extends AnimalAbstract  {
+    public Animal(String name, String age, String health, String type){
 
+        if (name.equals("")) {
+            throw new IllegalArgumentException("Please enter a name");
+        }
+        if (age.equals("")) {
+            throw new IllegalArgumentException("Please enter the age of the animal");
+        }
+        if (health.equals("")) {
+            throw new IllegalArgumentException("Please enter the health status of the animal");
+        }
 }

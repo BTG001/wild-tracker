b46abdd047a35e6eac6ed18e7c32879528a2737d
BTG001 <brunokipkorir2@gmail.com>
2020-07-25 15:48:00 +0300

    Create a constant ANIMAL_TYPE: Endangered

--- src/main/java/Endangered.java
+++ src/main/java/Endangered.java
@@ -1,8 +1,9 @@
-import org.sql2o.Connection;
+mport org.sql2o.Connection;
 
 import java.util.List;
 
 public class Endangered extends  AnimalAbstract {
+    private static final String ANIMAL_TYPE = "endangered";
     public Endangered(String name, String age, String health, String type) {
         if (name.equals("")) {
             throw new IllegalArgumentException("Please enter a name");
@@ -13,4 +14,9 @@ public class Endangered extends  AnimalAbstract {
         if (health.equals("")) {
             throw new IllegalArgumentException("Please enter the health status of the animal");
         }
+        this.name = name;
+        this.age = age;
+        this.health = health;
+        this.type = ANIMAL_TYPE;
+    }
 }

76704784fbaa6867efcb668551b0edcf97e7a520
BTG001 <brunokipkorir2@gmail.com>
2020-07-26 16:57:14 +0300

    Work on functionality

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,5 @@
 import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5433/wildlife_tracker", "buno", "Bruno11057");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5433/wildlife_tracker", "postgres", "Bruno11057");
 }
